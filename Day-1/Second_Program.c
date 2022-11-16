65c271fa619786456484940b116b3dcb34aaf25e
Aymene <aymanebagous04@gmail.com>
2022-11-16 16:39:00 +0100

    new file:   .vscode/settings.json 	new file:   Day-1/Second_Program 	modified:   Day-1/Second_Program.c 	modified:   Notes/notes-day-1.txt

--- Day-1/Second_Program.c
+++ Day-1/Second_Program.c
@@ -2,10 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 //uncomment each experiment and debug and run to see results
 
 int main(int argc, char* argv[]) 
 {
+    int id = fork(); 
+    int n;
+    if (id == 0)
+    {
+        n = 1;
+    }
+    else
+    {
+        n = 6;
+    }
+
+    if (id != 0)
+    {
+        wait(NULL);
+    }
+
+    int i;
+    for (i = n; i < n + 5; i++)
+    {
+        printf("%d ", i);
+        fflush(stdout);
+    }
+
+    if (id != 0)
+    {
+        printf("\n");
+    }
+
     return 0;
 }
