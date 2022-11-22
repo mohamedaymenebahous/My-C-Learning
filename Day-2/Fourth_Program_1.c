c342c082eed41ce503dec27a09ea8c191c312a3f
Aymene <aymanebagous04@gmail.com>
2022-11-22 03:10:48 +0100

    new file:   Day-2/Fourth_Program_1 	new file:   Day-2/Fourth_Program_1.c 	new file:   Day-2/Fourth_Program_2 	new file:   Day-2/Fourth_Program_2.c 	new file:   Day-2/a.out

---
+++ Day-2/Fourth_Program_1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
+
+//uncomment each experiment and debug and run to see results
+ 
+int main(int argc, char *argv[])
+{
+    int arr[5];
+    srand(time(NULL));
+    for (int i = 0; i < 5; i++)
+    {
+        arr[i] = rand() % 100; 
+        printf("Generated %d\n", arr[i]);
+    }
+
+    int fd = open("sum", O_WRONLY);
+    if (fd == -1)
+    {
+        return 1;
+    }
+
+    if (write(fd, arr, sizeof(int) * 5) == -1)
+    {
+        return 2;
+    }
+    
+    //alternative method
+    /*
+    for (int i = 0; i < 5; i++)
+    {
+        if (write(fd, &arr[i], sizeof(int)) == -1)
+        {
+            return 2;
+        }
+        printf("Wrote %d\n", arr[i]);
+    }*/
+
+    close(fd);
+
+    return 0;
+}
---
+++ Day-2/Fourth_Program_2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
+
+//uncomment each experiment and debug and run to see results
+
+int main(int argc, char *argv[])
+{
+    int arr[5];
+    int fd = open("sum", O_RDONLY);
+    if (fd == -1)
+    {
+        return 1;
+    }
+
+    if (read(fd, arr, sizeof(int) * 5) == -1)
+    {
+        return 2;
+    }
+
+    //alternative method
+    /*
+    for (int i = 0; i < 5; i++)
+    {
+        if (read(fd, &arr[i], sizeof(int)) == -1)
+        {
+            return 2;
+        }
+        printf("Received %d\n", arr[i]);
+    }
+    */
+
+    close(fd);
+
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        sum += arr[i];
+    }
+    printf("Result of the sum : %d\n", sum);
+
+    return 0;
+}
